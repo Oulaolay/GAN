e1cfe62326f9055d99656d6fcf1de17d7242ea49
Oulaolay <timothee.poulain@gmail.com>
2017-01-26 17:27:21 +0100

    update_GAN_3_histo_beautifull

--- GAN_gauss_2.lua
+++ GAN_gauss_2.lua
@@ -3,7 +3,9 @@
  require 'optim'
  require 'gnuplot'
  require 'utils'
-
+require 'distributions'
+require 'randomkit'
+require 'itorch'
  ---------------------------------------------------------
  ------------- COMMAND OPTIONS ---------------------------
  ---------------------------------------------------------
@@ -11,29 +13,32 @@
  cmd = torch.CmdLine()
  ------------- Algorithm ------------
  cmd:option('-batch_size',25,"mini-batch size")
- cmd:option('-maxEpoch',10000,"number of epochs")
+ cmd:option('-maxEpoch',5000,"number of epochs")
  cmd:option('-lr',1e-3,"learning rate")
- cmd:option('-step', 1, "number of discriminator training iteration for one generative training iteration")
+ cmd:option('-step', 10, "number of discriminator training iteration for one generative training iteration")
 
  ------------- Data -----------------
- cmd:option('-DIMENSION', 2, "dimension of the example data")
+ cmd:option('-DIMENSION', 1, "dimension of the example data")
  cmd:option('-n_points', 1000, "number of examples")
  cmd:option('-ratio',0.8,"train/total ratio. To split the dataset in train and test sets")
  cmd:option('-mean', 8, "mean of the Gaussian distribution to sample from")
  cmd:option('-var', 0.5, "variance of the Gaussian distribution to sample from")
  ------------ Model -----------------
- cmd:option('-noise_size', 2, "dimension of the noise vector")
+ cmd:option('-noise_size', 1, "dimension of the noise vector")
  cmd:option('-noise_type', "Gaussian", "either Gaussian or Uniform")
  cmd:option('-generative_size', 10, "dimension of the hidden layers of the generative model")
  cmd:option('-discrim_size',10, "dimension of the hidden layers of the discriminant model")
  cmd:option('-var_noise', 0.5, "mean of the noise distribution")
  cmd:option('-mean_noise', 4, "mean of the noise distribution")
+ cmd:option('-seed_value', 1010, "seed value for random generated data")
+
 
  local opt = cmd:parse(arg)
  print("GAN Implementation with Gaussian distributed data")
  print("Parameters of this experiment :")
  print(opt)
 
+ torch.manualSeed(opt.seed_value)
 batch_dep=opt.n_points/opt.batch_size -- Warning : Size of batch !
 -- Tirage d'une gaussienne
 local x_train_d=torch.Tensor(opt.n_points,opt.DIMENSION)
@@ -43,12 +48,15 @@ end
 
 z_noise=torch.Tensor(batch_dep,opt.noise_size)
 label_dat=torch.Tensor(batch_dep,opt.DIMENSION)
+label_dat=torch.Tensor(batch_dep,opt.DIMENSION)
+label_dat_test=torch.Tensor(x_train_d:size(1),opt.DIMENSION)
+label_dat_false_test=torch.Tensor(x_train_d:size(1),opt.DIMENSION)
+
 label_dat_false=torch.Tensor(batch_dep,opt.DIMENSION)
 z_noise_final=torch.Tensor(opt.n_points,opt.noise_size)
-print(z_noise_final:size())
 -- Creating noise
 function create_noise()
-  for i=1,batch_dep/2 do
+  for i=1,batch_dep do
     z_noise[i]:copy(torch.randn(opt.noise_size)*opt.var_noise+opt.mean_noise)
   end
      return z_noise
@@ -66,10 +74,28 @@ end
 for i=1,batch_dep do
   label_dat_false[i]=1
 end
+
+
+--Creating labels real data for discriminative model
+for i=1,x_train_d:size(1)/2 do
+  label_dat_test[i]=1
+end
+--Creating labels fake data for discriminative model
+for i=x_train_d:size(1)/2+1,x_train_d:size(1) do
+  label_dat_test[i]=0
+end
+--Creating labels fake data generative model
+for i=1,x_train_d:size(1) do
+  label_dat_false_test[i]=1
+end
+
 -- Creating noise for eval
+function Create_noise_final()
   for i=1,opt.n_points do
      z_noise_final[i]:copy(torch.randn(opt.noise_size)*opt.var_noise+opt.mean_noise)
   end
+  return z_noise_final
+end
 
 -------------------------------------------
 --------------CREATE MODEL G----------------
@@ -99,30 +125,65 @@ function Prepare_batch()
       return x_batch_d
 end
 
+function Prepare_real_data_test()
+      local x_test=torch.Tensor(opt.n_points,opt.DIMENSION)
+      local shuffle = torch.randperm(x_train_d:size(1))
+      local x_test_chunk = shuffle:chunk(x_train_d:size(1),1)
+      for index = 1,x_train_d:size(1) do
+          x_test[index] =x_train_d[shuffle[index]]
+      end
+      return x_test
+end
+
+
+
 --------------------------------------------
 --------------TESTING ----------------------
 --------------------------------------------
-
+gnuplot.axis({2,10,0,0.1})
 function Eval_model(i)
-  if(i%10==0) then
-    output=model_g:forward(z_noise_final)
-    if(x_train_d:size()==1) then
-      gnuplot.hist(output)
-    else
-      gnuplot.plot({output,'+'},{x_train_d,'+'})
+  -- Evaluate data with
+  --print(Create_noise_final())
+  local data_gen_test=model_g:forward(Create_noise_final())
+  x_test=Prepare_real_data_test()
+  x_test:sub(x_test:size(1)/2+1,x_test:size(1)):copy(data_gen_test:sub(1,x_test:size(1)/2))
+
+  local output=model_d:forward(x_test)
+  local loss_d_test=criterion:forward(output,label_dat_test)
+  table.insert(all_losses_d_test,loss_d_test)
+  --gnuplot.plot(torch.Tensor(all_losses_d_test))
+  local output_fake=model_d:forward(data_gen_test)
+  local loss_g_test=criterion:forward(output_fake,label_dat_false_test)
+  table.insert(all_losses_g_test,loss_g_test)
+  --print(loss_g_test)
+  --gnuplot.plot(torch.Tensor(all_losses_g_test),torch.Tensor(all_losses_d_test))
+
+
+  if(i%1==0) then
+    if(opt.DIMENSION==1) then
+      display_decision=torch.Tensor(200,2)
+      for i=1,200 do
+        display_decision[i][1]=0+(10-0)/200*(i-1)
+        display_decision[i][2]=model_d:forward(torch.Tensor(1):fill(display_decision[i][1]))/10
+      end
+      gnuplot.plot({histogram(x_train_d,25),'+-'},{histogram(data_gen_test,25),'+-'},{display_decision,'+-'})
+      --plot = Plot():histogram(torch.randn(10000)):draw()
+
     end
   end
+
 end
 
 
 ---------------------------------------------
 -------------- TRAINNING  -------------------
 ---------------------------------------------
-local criterion=nn.BCECriterion()
+criterion=nn.BCECriterion()
 
-local all_losses={}
-local all_losses_g={}
-
+all_losses={}
+all_losses_g={}
+all_losses_d_test={}
+all_losses_g_test={}
 for iteration=1,opt.maxEpoch do
   if(iteration%2000==0) then
     opt.lr=opt.lr/2
@@ -131,6 +192,7 @@ for iteration=1,opt.maxEpoch do
       x_batch_d=Prepare_batch()
       loss_d=0
       loss_g=0
+      Eval_model(iteration)
       for i=1,opt.batch_size do
         model_d:zeroGradParameters()
         -- Forward noise
@@ -157,7 +219,6 @@ for iteration=1,opt.maxEpoch do
       --gnuplot.plot(torch.Tensor(all_losses))
       if iteration%100 == 0 then
        print("Achievement : " .. iteration/opt.maxEpoch*100 .. "%")
-       Eval_model(iteration)
     end
 
     end
@@ -174,7 +235,7 @@ for iteration=1,opt.maxEpoch do
       -- Forward data_gen in model D
       output=model_d:forward(data_gen)
       -- Forward avec le criterion BCE
-      loss_g=criterion:forward(output,label_dat_false)
+      --loss_g=criterion:forward(output,label_dat_false)
       delta=criterion:backward(output,label_dat_false)
       -- update for back_prop data_gen
       delta_d=model_d:backward(data_gen,delta)
